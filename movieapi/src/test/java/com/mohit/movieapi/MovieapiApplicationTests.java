ee5547246631d331dfa917a5d2f36a811e4ea167
Error-NaN <mohitnagaraj20@gmail.com>
2023-01-28 15:46:53 +0530

    adding files

---
+++ movieapi/src/test/java/com/mohit/movieapi/MovieapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
