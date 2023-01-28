4a11abd4778b993246135d924d9b36f318696f28
Error-NaN <mohitnagaraj20@gmail.com>
2023-01-28 16:44:22 +0530

    2nd part of vid

--- movieapi/src/main/java/com/mohit/movieapi/MovieapiApplication.java
+++ movieapi/src/main/java/com/mohit/movieapi/MovieapiApplication.java
@@ -2,14 +2,14 @@ package com.mohit.movieapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController 
 public class MovieapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieapiApplication.class, args);
 	}
+
 
 }
